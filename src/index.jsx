ebb1589255532805a2b91b4ec16d32f40f3919bd
thomas <19903766258@163.com>
2023-08-29 22:59:05 +0800

    feat: REM 响应式布局

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.less";
-import App from "./App";
+//import App from "./App";
+
+/* 改变 REM 换算比例 */
+import "lib-flexible";
+import "./index.less";
+
+/* 处理 REM 最大宽度 */
+(function () {
+  const handleMax = function handleMax() {
+    let html = document.documentElement,
+      root = document.getElementById("root"),
+      deviceW = html.clientWidth;
+    root.style.maxWidth = "750px";
+    if (deviceW >= 750) {
+      html.style.fontSize = "75px";
+    }
+  };
+  handleMax();
+})();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <div className="box">知乎日报</div>
+  </>
 );

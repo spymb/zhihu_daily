7bba1e6014abfa09d205512173f601d02cc29714
spym <spym200@qq.com>
2023-09-02 11:29:44 +0800

    feat: 首页头部结构与样式

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import HomeHead from "../components/HomeHead";
+import _ from "../assets/utils";
 
 const Home = () => {
-  return <div>首页</div>;
+  const [today, setToday] = useState(_.formatTime(null, "{0}{1}{2}"));
+
+  return (
+    <div className="home-box">
+      {/* 头部 */}
+      <HomeHead today={today} />
+    </div>
+  );
 };
 
 export default Home;

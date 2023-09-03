e0fd2ada374b0021732a1db1dd239ce791dbb293
spym <spym200@qq.com>
2023-09-03 13:47:54 +0800

    feat: 文章详情页渲染

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -1,7 +1,119 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Detail.less";
+import {
+  LeftOutline,
+  MessageOutline,
+  LikeOutline,
+  StarOutline,
+  MoreOutline,
+} from "antd-mobile-icons";
+import { Badge } from "antd-mobile";
+import api from "../api";
+import SkeletonAgain from "../components/SkeletonAgain";
+import { flushSync } from "react-dom";
 
-const Detail = () => {
-  return <div>新闻详情</div>;
+const Detail = function Detail(props) {
+  let { navigate, params } = props;
+  /* 定义状态 */
+  const [info, setInfo] = useState(null);
+  const [extra, setExtra] = useState(null);
+
+  let link;
+  const handleStyle = (result) => {
+    let { css } = result;
+    if (!Array.isArray(css)) return;
+    css = css[0];
+    if (!css) return;
+
+    // 创建<LINK>导入样式
+    link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = css;
+    document.head.appendChild(link);
+  };
+  const handleImage = (result) => {
+    let imgPlaceHolder = document.querySelector(".img-place-holder");
+    if (!imgPlaceHolder) return;
+    // 创建大图
+    let tempImg = new Image();
+    tempImg.src = result.image;
+    tempImg.onload = () => {
+      imgPlaceHolder.appendChild(tempImg);
+    };
+    tempImg.onerror = () => {
+      let parent = imgPlaceHolder.parentNode;
+      parent.parentNode.removeChild(parent);
+    };
+  };
+
+  /* 第一次渲染完毕:获取数据 */
+  useEffect(() => {
+    (async () => {
+      try {
+        let result = await api.queryNewsInfo(params.id);
+        flushSync(() => {
+          setInfo(result);
+          handleStyle(result);
+        });
+        handleImage(result);
+      } catch (_) {}
+    })();
+    // 销毁组件:移除创建的样式
+    return () => {
+      if (link) document.head.removeChild(link);
+    };
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        let result = await api.queryStoryExtra(params.id);
+        setExtra(result);
+      } catch (_) {}
+    })();
+  }, []);
+
+  return (
+    <div className="detail-box">
+      {/* 新闻内容 */}
+      {!info ? (
+        <SkeletonAgain />
+      ) : (
+        <div
+          className="content"
+          dangerouslySetInnerHTML={{
+            __html: info.body,
+          }}
+        ></div>
+      )}
+
+      {/* 底部图标 */}
+      <div className="tab-bar">
+        <div
+          className="back"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <LeftOutline />
+        </div>
+        <div className="icons">
+          <Badge content={extra ? extra.comments : 0}>
+            <MessageOutline />
+          </Badge>
+          <Badge content={extra ? extra.popularity : 0}>
+            <LikeOutline />
+          </Badge>
+          <span>
+            <StarOutline />
+          </span>
+          <span>
+            <MoreOutline />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;

7309b8adf4afb9188b873d659be5a1e603936b0a
spym <spym200@qq.com>
2023-09-04 22:16:56 +0800

    feat: 内部封装了 loading 状态的 ButtonAgain 组件

--- src/components/ButtonAgain.tsx
+++ src/components/ButtonAgain.tsx
@@ -1,8 +1,31 @@
-import React from "react";
-import "./ButtonAgain.less";
+import React, { useState } from "react";
+import { Button } from "antd-mobile";
 
-const ButtonAgain = function ButtonAgain() {
-  return <div>button</div>;
+const ButtonAgain = function ButtonAgain(props) {
+  /* props中包含了调用<Button>组件时候的属性 */
+  let options = { ...props };
+  const { children, onClick } = options;
+  delete options.children;
+
+  /* 状态 */
+  let [loading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+    try {
+      await onClick();
+    } catch (_) {}
+    setLoading(false);
+  };
+
+  if (onClick) {
+    options.onClick = handleClick;
+  }
+
+  return (
+    <Button {...options} loading={loading}>
+      {children}
+    </Button>
+  );
 };
-
 export default ButtonAgain;

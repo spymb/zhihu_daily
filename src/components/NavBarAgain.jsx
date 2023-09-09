baa14b75c32b0c0cacfdd29f15f91398338aed83
spym <spym200@qq.com>
2023-09-09 15:27:04 +0800

    feat: navBar 回退逻辑

--- src/components/NavBarAgain.jsx
+++ src/components/NavBarAgain.jsx
@@ -2,11 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 import { NavBar } from "antd-mobile";
 import "./NavBarAgain.less";
+import { useNavigate, useLocation, useSearchParams } from "react-router-dom";
 
 const NavBarAgain = function NavBarAgain(props) {
   let { title } = props;
 
-  const handleBack = () => {};
+  const navigate = useNavigate(),
+    location = useLocation(),
+    [usp] = useSearchParams();
+
+  const handleBack = () => {
+    // 特殊:登录页 & to的值是/deatil/xxx
+    let to = usp.get("to");
+    if (location.pathname === "/login" && /^\/detail\/\d+$/.test(to)) {
+      navigate(to, { replace: true });
+      return;
+    }
+    navigate(-1);
+  };
 
   return (
     <NavBar className="navbar-again-box" onBack={handleBack}>

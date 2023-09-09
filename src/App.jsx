ff1d176140ee3fd74307f20f3bd9d77b3058b15a
spym <spym200@qq.com>
2023-09-09 21:35:37 +0800

    feat: 首页组件缓存

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { HashRouter } from "react-router-dom";
 import RouterView from "./router";
+import { KeepAliveProvider } from "keepalive-react-component";
 
 const App = function App() {
   return (
     <HashRouter>
-      <RouterView />
+      <KeepAliveProvider>
+        <RouterView />
+      </KeepAliveProvider>
     </HashRouter>
   );
 };
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,12 @@
 import { lazy } from "react";
+import Home from '../views/Home';
+import { withKeepAlive } from "keepalive-react-component";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: lazy(() => import("../views/Home")),
+    component: withKeepAlive(Home, { cacheId: "home", scroll: true }),
     meta: {
       title: "知乎日报-WebApp",
     },
